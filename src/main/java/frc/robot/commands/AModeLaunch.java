8ba1ed790a936de2c214e6dcede30ad1c2899e77
TroBots <wlhsroboticsteam@gmail.com>
2022-02-25 18:21:26 -0500

    mg - update extendroller on Launchmode

--- src/main/java/frc/robot/commands/AModeLaunch.java
+++ src/main/java/frc/robot/commands/AModeLaunch.java
@@ -10,7 +10,7 @@ public class AModeLaunch extends ParallelCommandGroup {
         //addCommands(new CmdRobotInit());
 
         //set retractor conditon
-        addCommands(new CmdMoveExtender(16));
+        addCommands(new CmdMoveExtender(22));
         
         //set indexer condition
         addCommands(new CmdIndexerStartCollecting());
