7882d1721f424fe937800d03665f7a816c53aea5
TroBots <wlhsroboticsteam@gmail.com>
2022-02-26 15:02:48 -0500

    HF-comp Tuning

--- src/main/java/frc/robot/commands/AutoGrp_2BallsV3.java
+++ src/main/java/frc/robot/commands/AutoGrp_2BallsV3.java
@@ -9,32 +9,29 @@ import frc.robot.commands.CmdIndexerLaunch;
 import frc.robot.subsystems.SubIntake;
 
 
-public class AutoGrp_2BallsV2 extends SequentialCommandGroup {
+public class AutoGrp_2BallsV3 extends SequentialCommandGroup {
     public final double TURN_SPEED = 0.2;
     public final double STRAIGHT_SPEED = 0.3;
     
-    public AutoGrp_2BallsV2() {
+    public AutoGrp_2BallsV3() {
         addCommands(new CmdRobotInit());
         //Start flywheel
-        
-
-        
+        addCommands(new CmdLauncherRun(2000));
+        addCommands(new CmdDelay(1.0));
         addCommands(new CmdIndexerStartCollecting());
-        addCommands(new CmdLauncherRun(2800));
-        addCommands(new CmdMoveExtender());
-        addCommands(new CmdAutoDriveStraght(60, 0, STRAIGHT_SPEED));
-        addCommands(new CmdMoveExtender(16));
-        addCommands(new CmdTurnByGyro(158,TURN_SPEED, -TURN_SPEED));
-        
-       
+        addCommands(new CmdDelay(.25));
         addCommands(new CmdIndexerLaunch());
         addCommands(new CmdDelay(.75));
-        addCommands(new CmdDelay(0.5));
+        addCommands(new CmdLauncherRun(2800));
+        addCommands(new CmdTurnByGyro(-177, TURN_SPEED, -TURN_SPEED));   
+        addCommands(new CmdMoveExtender());
+        addCommands(new CmdAutoDriveStraght(60, 180, STRAIGHT_SPEED));
+        addCommands(new CmdMoveExtender(16));
+        addCommands(new CmdAutoDriveStraght(20, 180, -STRAIGHT_SPEED));
+        addCommands(new CmdTurnByGyro(0,-TURN_SPEED, TURN_SPEED));
         addCommands(new CmdIndexerLaunch());
         addCommands(new CmdDelay(.5));
         addCommands(new CmdLauncherStop());
         addCommands(new CmdMoveExtender(0));
-        addCommands(new CmdIndexerStop());
-        
     }
 }
