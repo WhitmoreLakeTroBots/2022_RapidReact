339a5253c576220d6608eb01e57a583d6c4e87d1
Jeff <xjschwen@gmail.com>
2022-03-01 20:18:41 -0500

    jas brought over edites from turnbygyro2 over to turnbylime

--- src/main/java/frc/robot/commands/CmdTurnByLime.java
+++ src/main/java/frc/robot/commands/CmdTurnByLime.java
@@ -2,36 +2,55 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
-
+import frc.robot.RobotMath;
 import frc.robot.subsystems.SubDriveTrain;
 import frc.robot.subsystems.SubGyro;
+import frc.robot.CommonLogic;
+
 import frc.robot.subsystems.SubLimelight;
+import frc.robot.subsystems.SubLimelight.CAM_MODE;
 import frc.robot.Constants.limelightConstants.cameras;
 
 public class CmdTurnByLime extends CommandBase {
 
-    
+    // private final SubDriveTrain subDriveTrain;
     private boolean bDone = false;
-
-    private double _requestedHeading = 0.0;
-    private double _left_throttle = 0.0;
-    private double _right_throttle = 0.0;
-    private double tol = 5.0;
-
     private SubDriveTrain subDriveTrain = null;
     private SubGyro subGyro = null;
-    private cameras camera2lookthrough = null;
+
+    private double _requestedHeading = 0.0;
+    private double _currHeading = 0.0;
+    private double _leftTargetThrottle = 0.0;
+    private double _rightTargetThrottle = 0.0;
+    private double TOL = 1.0;
+    private final double MIN_THROTTLE = 0.04;
+    private double _KPLeft = 0.0;
+    private double _KPRight = 0.0;
+    private double _minLeftThrottle = 0.0;
+    private double _minRightThrottle = 0.0;
     private int camPipeline = 0;
+    private cameras camera2lookthrough = null;
     private SubLimelight camera = null;
 
     public CmdTurnByLime(double heading_deg, double left_throttle, double right_throttle, cameras cam, int pipeline) {
 
         _requestedHeading = heading_deg;
-        _left_throttle = left_throttle;
-        _right_throttle = right_throttle;
+        _leftTargetThrottle = left_throttle;
+        _rightTargetThrottle = right_throttle;
         camPipeline = pipeline;
         camera2lookthrough = cam;
 
+    }
+
+    public CmdTurnByLime(double heading_deg, double left_throttle, double right_throttle, cameras cam, int pipeline,
+            double tol_degrees) {
+
+        _requestedHeading = heading_deg;
+        _leftTargetThrottle = left_throttle;
+        _rightTargetThrottle = right_throttle;
+        camPipeline = pipeline;
+        camera2lookthrough = cam;
+        TOL = tol_degrees;
     }
 
     // Called when the command is initially scheduled.
@@ -39,10 +58,7 @@ public class CmdTurnByLime extends CommandBase {
     public void initialize() {
         bDone = false;
         subDriveTrain = RobotContainer.getInstance().subDriveTrain;
-
         subGyro = RobotContainer.getInstance().subGyro;
-        subDriveTrain.resetBothEncoders();
-        subDriveTrain.Drive(_left_throttle, _right_throttle);
 
         if (camera2lookthrough == cameras.limelight_high) {
             camera = RobotContainer.getInstance().subLimelightHigh;
@@ -52,6 +68,13 @@ public class CmdTurnByLime extends CommandBase {
 
         camera.setPipeline(camPipeline);
         camera.setCamMode(SubLimelight.CAM_MODE.VISION_PROCESSING);
+        _minLeftThrottle = calcMinThrottle(_leftTargetThrottle, MIN_THROTTLE);
+        _minRightThrottle = calcMinThrottle(_rightTargetThrottle, MIN_THROTTLE);
+
+        double headingDelta = RobotMath.headingDelta(_currHeading, _requestedHeading);
+        _KPLeft = calcKP(_leftTargetThrottle, _minLeftThrottle, headingDelta);
+        _KPRight = calcKP(_rightTargetThrottle, _minRightThrottle, headingDelta);
+
 
     }
 
@@ -65,7 +88,17 @@ public class CmdTurnByLime extends CommandBase {
             _requestedHeading = subGyro.gyroNormalize(subGyro.getNormaliziedNavxAngle() + camera.getTX());
         }
 
-        if (subGyro.gyroInTol(subGyro.getNormaliziedNavxAngle(), _requestedHeading, tol)) {
+        // Might need to pub logic here to cause the robot to spin in the other direction 
+        // AKA reverse the direction of spin if the new _requested heading in getting larger...
+
+        
+        double currHeading = subGyro.getNormaliziedNavxAngle();
+        double headingDelta = RobotMath.headingDelta(currHeading, _requestedHeading);
+        double powerLeft = calcMotorPower(_leftTargetThrottle, _minLeftThrottle, _KPLeft, headingDelta);
+        double powerRight = calcMotorPower(_rightTargetThrottle, _minRightThrottle, _KPRight, headingDelta);
+        subDriveTrain.Drive(powerLeft, powerRight);
+
+        if (subGyro.gyroInTol(subGyro.getNormaliziedNavxAngle(), _requestedHeading, TOL)) {
             bDone = true;
             end(false);
         }
@@ -90,4 +123,49 @@ public class CmdTurnByLime extends CommandBase {
         // this always returns false;
         return false;
     }
+
+    // converts everything to a min throttle keeping the original sign
+    private double calcMinThrottle(double throttle, double minThrottle) {
+        return Math.signum(throttle) * minThrottle;
+    }
+
+    // calculates a KP based on (throttle - min throttle) / heading delta
+    private double calcKP(double throttle, double minThrottle, double deltaHeading) {
+        // Math Time.... Based on 1 second for 360 degree turns.
+
+        // Assume robot has 14ft/sec = (14*12)in/sec = 168 inch/sec
+        // Assume robot has track width of 24 inches
+        // circumference of the circle = Math.pi * 24 = 75.4 inches
+        // 75.4 / 168 means that motor power of .448 should give us a 1 second 360
+        // degree turn.
+
+        double theroyMaxSpeed = 14 * 12;
+        double trackwidth = 24;
+        double full360turndist = trackwidth * Math.PI;
+        double full360throttle = full360turndist / theroyMaxSpeed;
+
+        double retValue = Math.signum(throttle) * Math.abs(full360throttle / 360);
+
+        // System.out.println("retValue = " + retValue);
+        return retValue;
+    }
+
+    // calculates the motor power and scales it based on the heading delta
+    private double calcMotorPower(double targetThrottle, double minThrottle, double KP, double headingDelta) {
+        double sigNum = Math.signum(targetThrottle);
+
+        double retValue = sigNum * (Math.abs(minThrottle) + (Math.abs(KP) * Math.abs(headingDelta)));
+
+        if (sigNum < 0) {
+            retValue = CommonLogic.CapMotorPower(retValue, targetThrottle, 0.0);
+        }
+
+        else if (sigNum > 0) {
+            retValue = CommonLogic.CapMotorPower(retValue, 0.0, targetThrottle);
+        } else {
+            retValue = 0.0;
+        }
+        return retValue;
+    }
+
 }
