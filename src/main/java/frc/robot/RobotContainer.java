8701121c1f70aff7964ea68ccf983c6503309d85
TroBots <wlhsroboticsteam@gmail.com>
2022-04-13 18:39:28 -0400

    HF button change

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -233,10 +233,10 @@ public class RobotContainer {
     // Xbox.lt.whenHeld(new CmdReverseRoller());
     // Xbox.lt.whenReleased(new CmdForwardRoller());
 
-    Xbox.lt.whenPressed(new CmdStopRoller());
+    //Xbox.lt.whenPressed(new CmdStopRoller());
     // Xbox.lb.whenPressed(new CmdStopRoller());
-    Xbox.lb.whenHeld(new CmdReverseRoller());
-    Xbox.lb.whenReleased(new CmdForwardRoller());
+    Xbox.lt.whenHeld(new CmdReverseRoller());
+    Xbox.lt.whenReleased(new CmdForwardRoller());
 
     Xbox.dPad.up.whenPressed(new CmdLauncherRun(2675.0));
     Xbox.dPad.left.whenPressed(new CmdLauncherRun(1500.0));
